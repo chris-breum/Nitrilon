2ce8df1dad656bdd442995b1cad8dfd308ae51d8
chris-breum <36517@edu.campusvejle.dk>
2024-04-11 10:31:02 +0200

    greated event class

---
+++ src/Nitilon/Nitrilon.Entities/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace Nitrilon.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public int Attendees { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+    }
+}
