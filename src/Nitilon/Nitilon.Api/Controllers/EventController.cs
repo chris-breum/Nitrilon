6ec7ae8397ff10a5a4634a8464e4c69d02154907
chris-breum <36517@edu.campusvejle.dk>
2024-04-12 09:31:04 +0200

    Have made GeAllEvent

--- src/Nitilon/Nitilon.Api/Controllers/EventController.cs
+++ src/Nitilon/Nitilon.Api/Controllers/EventController.cs
@@ -18,33 +18,9 @@ namespace Nitilon.Api.Controllers
         [HttpGet]
         public IEnumerable<Event> GetAll()
         {
-            List<Event> events = new List<Event>
-            {
-                new Event
-                {
-                    Id = 0,
-                    Date = DateTime.Today,
-                    Name = "Birthday Party",
-                    Attendees = 5,
-                    Description = "Birthday party for John"
-                },
-                new Event
-                {
-                    Id = 1,
-                    Date = DateTime.Today,
-                    Name = "Wedding",
-                    Attendees = 9,
-                    Description = "Wedding of John and Jane"
-                },
-                new Event
-                {
-                    Id = 2,
-                    Date = DateTime.Today,
-                    Name = "Graduation",
-                    Attendees = 1,
-                    Description = "Graduation of John"
-                }
-            };
+           Repository r = new();
+            var events = r.GetAllEvent();
+
             return events;
         }
 
--- src/Nitilon/Nitrilon.DataAccess/Repository.cs
+++ src/Nitilon/Nitrilon.DataAccess/Repository.cs
@@ -8,6 +8,66 @@ namespace Nitrilon.DataAccess
     public class Repository
     {
         private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = NitrilonDB;Integrated Security = True; Connect Timeout = 30; Encrypt=True;Trust Server Certificate=False;Application Intent = ReadWrite; Multi Subnet Failover=False";
+        public List<Event> GetAllEvent()
+        {
+            List<Event> events = new List<Event>();
+            string sql = "SELECT * FROM Events";
+            // Do that db stuff
+
+            //1: make a sql connection object
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            //2: make a sqlcommand object
+
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            //3: open the connection
+
+            connection.Open();
+
+            //4: Execute Query
+            SqlDataReader reader = command.ExecuteReader();
+            //5: Read the results
+            while (reader.Read())
+            {
+                //Event e = new Event();
+                //e.Id = reader.GetInt32(0);
+                //e.Date = reader.GetDateTime(1);
+                //e.Name = reader.GetString(2);
+                //e.Attendees = reader.GetInt32(3);
+                //e.Description = reader.GetString(4);
+                //events.Add(e);
+
+                int id = Convert.ToInt32(reader["EventId"]);
+                DateTime date = Convert.ToDateTime(reader["Date"]);
+                string name = reader["Name"].ToString();
+                int attendees = Convert.ToInt32(reader["Attendees"]);
+                string description = reader["Description"].ToString();
+
+
+                Event e = new()
+                {
+                    Id = id,
+                    Date = date,
+                    Name = name,
+                    Attendees = attendees,
+                    Description = description;
+
+            }
+            connection.Close();
+
+
+            return events;
+
+        }
+
+
+
+
+
+
+
+
 
         public int Save(Event newEvent)
         {
@@ -40,5 +100,7 @@ namespace Nitrilon.DataAccess
 
 
         }
+
     }
 }
+
